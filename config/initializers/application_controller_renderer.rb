2c6869146e54fc1416b6fdec1134f34fb6b36fb9
ochiaihitomi <sky.force.1013@gmail.com>
2021-01-15 04:37:08 +0000

    [Start] 4-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3d255f4bf82e45c8ff1613924d39eefda65acf1fcdd5016ca21d9fdf9fa67ca5a0207fc3e0be66eeed67ad73e27428a1e368e8a983aebf51ebad77e8903e306a'
